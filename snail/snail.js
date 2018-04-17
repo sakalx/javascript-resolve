c6d47593944385a205903168d94306d0e3374059
sakal <sakals@mail.ua>
2018-04-17 12:12:42 -0400

    assets: img for task Snail

---
+++ snail/snail.js
@@ -0,0 +1,25 @@
+function snail(arr) {
+    const res = [];
+    res.push(...arr.shift());
+
+    (function spliceArr() {
+        arr.forEach((subAr, i) => {
+            (i !== arr.length - 1)
+                ? res.push(subAr.pop())
+                : res.push(...arr.pop().reverse())
+        });
+        arr.reverse();
+        arr.forEach(subAr => subAr.reverse());
+
+        arr.length !== 0 && spliceArr();
+    })();
+    return res;
+}
+
+array = [
+    [1,2,3],
+    [8,9,4],
+    [7,6,5]
+];
+
+console.log(snail(array)); // [1,2,3,4,5,6,7,8,9]
